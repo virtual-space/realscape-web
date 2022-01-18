5dd4e03500d14ee99a9fc4779462a413cfde044d
Marko Laban <marko.laban@virtualspace.io>
2022-01-18 13:55:51 +1300

    [RS-27] added local env

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  api: 'http://localhost:8080',
+  client_id: 'IEmf5XYQJXIHvWcQtZ5FXbLM',
+  appVersion: require('../../package.json').version + '-local'
+};
