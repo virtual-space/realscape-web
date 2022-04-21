59f0745332697b8034ccf7f467eed8bd50fc08a3
Samuel-Boyes-l33t <sam.boyes@l33tsystems.com>
2022-04-21 14:05:31 +1200

    Update environment and gitignore.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,5 +3,6 @@ export const environment = {
   api: 'https://api-dev.realnet.io',
   home: 'https://dev.realscape.io',
   client_id: 'IEmf5XYQJXIHvWcQtZ5FXbLM',
-  appVersion: require('../../package.json').version + '-dev'
+  appVersion: require('../../package.json').version + '-dev',
+  mapboxToken: 'pk.eyJ1IjoidjFydHU0bHNwNGMzIiwiYSI6ImNreW1mZXI5ZzA2aHQydG5zY2hiNWh0ZjAifQ.BovKKecsXgBq1wu7PlAUHQ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   api: 'http://localhost:8080',
   home: 'http://localhost:4200',
   client_id: 'IEmf5XYQJXIHvWcQtZ5FXbLM',
-  appVersion: require('../../package.json').version + '-local'
+  appVersion: require('../../package.json').version + '-local',
+  mapboxToken: 'pk.eyJ1IjoidjFydHU0bHNwNGMzIiwiYSI6ImNreW1mZXI5ZzA2aHQydG5zY2hiNWh0ZjAifQ.BovKKecsXgBq1wu7PlAUHQ'
 };
 
 /*
