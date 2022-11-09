fe58431bf1900d75400718dcf0d28459b8c4a354
Bojana Zivkovic <bojana.zivkovic@l33tsystems.com>
2022-11-09 15:23:46 +1300

    [RS-280] Adding satellite mode to map

--- src/app/rn-map-view/rn-map-view.component.ts
+++ src/app/rn-map-view/rn-map-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewEncapsulation, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Map, NavigationControl, Marker, SymbolLayout, LngLat, Popup, LngLatBounds, LngLatBoundsLike, } from 'mapbox-gl';
+import { StylesControl } from 'mapbox-gl-controls';
 import { Subscription } from 'rxjs';
 // @ts-ignore
 import * as MapboxDraw from "mapbox-gl-draw";
@@ -13,7 +14,8 @@ import { RnCardViewComponent } from '../rn-card-view/rn-card-view.component';
 @Component({
   selector: 'app-rn-map-view',
   templateUrl: './rn-map-view.component.html',
-  styleUrls: ['./rn-map-view.component.sass']
+  styleUrls: ['./rn-map-view.component.sass'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class RnMapViewComponent extends RnViewComponent implements OnInit, OnDestroy {
 
@@ -28,7 +30,7 @@ export class RnMapViewComponent extends RnViewComponent implements OnInit, OnDes
   isLoaded = false;
   subscription?: Subscription;
   map?: Map;
-  style = 'mapbox://styles/mapbox/streets-v11';
+  style = 'mapbox://styles/mapbox/streets-v11?optimize=true';
   lat = 45.899977;
   lng = 6.172652;
 
@@ -137,6 +139,21 @@ export class RnMapViewComponent extends RnViewComponent implements OnInit, OnDes
         zoom: this.zoom,
         center: [this.lng, this.lat]
       });
+      this.map.addControl(new StylesControl({
+        styles: [
+          {
+            label: 'Streets',
+            styleName: 'Mapbox Streets',
+            styleUrl: 'mapbox://styles/mapbox/streets-v11?optimize=true',
+          }, {
+            label: 'Satellite',
+            styleName: 'Satellite',
+            styleUrl: 'mapbox://styles/mapbox/satellite-v9?optimize=true',
+          },
+        ],
+       //onChange: (style) => console.log(style),
+      }), 'top-left');
+
       this.map.addControl(new NavigationControl());
         this.draw = new MapboxDraw(
           {
@@ -511,7 +528,7 @@ export class RnMapViewComponent extends RnViewComponent implements OnInit, OnDes
               if (icon) {
                 customMarkerIcon = document.createElement('div');
                 customMarkerIcon.className = 'marker';
-                customMarkerIcon.innerHTML = `<div fxLayout='column' fxLayoutAlign='center center'><div class="material-icons">${icon}</div><div>${ip.name}</div></div>`
+                customMarkerIcon.innerHTML = `<div id='markerText' class='center'><i class='material-icons'>${icon} </i><span>${ip.name}</span></div>`
               }
               const m = new Marker(customMarkerIcon);
               this.markers.push(m);
@@ -544,7 +561,7 @@ export class RnMapViewComponent extends RnViewComponent implements OnInit, OnDes
                     }
                   }
                 });
-                console.log('adding source ', 's' + ip.id);
+                //console.log('adding source ', 's' + ip.id);
                 this.sources.push('s' + ip.id);
                 map.addLayer({
                   'id': 'l' + ip.id,
@@ -556,7 +573,7 @@ export class RnMapViewComponent extends RnViewComponent implements OnInit, OnDes
                     'fill-opacity': 0.5
                   }
                 });
-                console.log('adding layer ', 'l' + ip.id);
+                //console.log('adding layer ', 'l' + ip.id);
                 this.layers.push('l' + ip.id);
                 map.on('click', 'l' + ip.id, (e) => {
                   if (e) {
@@ -573,14 +590,14 @@ export class RnMapViewComponent extends RnViewComponent implements OnInit, OnDes
                     'line-width': 3
                   }
                 });
-                console.log('adding layer ', 'l' + ip.id + 'outline');
+                //console.log('adding layer ', 'l' + ip.id + 'outline');
                 this.layers.push('l' + ip.id + 'outline');
                 let customMarkerIcon = undefined;
                 let icon = this.getItemIcon(ip);
                 if (icon) {
                   customMarkerIcon = document.createElement('div');
                   customMarkerIcon.className = 'marker';
-                  customMarkerIcon.innerHTML = `<div fxLayout='column' fxLayoutAlign='center center'><div class="material-icons">${icon}</div><div>${ip.name}</div></div>`
+                  customMarkerIcon.innerHTML = `<div id='markerText' class='center'><i class='material-icons'>${icon}</i><span>${ip.name}</span></div>`
                 }
                 const m = new Marker(customMarkerIcon);
                 this.markers.push(m);
@@ -638,7 +655,7 @@ export class RnMapViewComponent extends RnViewComponent implements OnInit, OnDes
   }
 
   override itemsChanged(event: any) {
-    console.log('map view items changed', event, 'changing', this.markersChanging);
+    //console.log('map view items changed', event, 'changing', this.markersChanging);
     if (this.markersChanging == false) {
       //this.markersChanging = true;
       //console.log(this.markers);
