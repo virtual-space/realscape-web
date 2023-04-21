89c8c721d135a5903391485cc9df5af4ce755742
Marko Laban <marko.laban@virtualspace.io>
2023-04-21 13:52:57 +1200

    updating prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://api.realnet.io',
+  api: 'https://realnet.io',
   home: 'https://www.realscape.io',
   client_id: 'IEmf5XYQJXIHvWcQtZ5FXbLM' ,
   appVersion: require('../../package.json').version,
