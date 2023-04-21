9fa511f9a23e925e7f9b65a8753c7f64d138ecdf
Marko Laban <marko.laban@virtualspace.io>
2023-04-21 15:18:25 +1200

    fixing prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://realnet.io',
+  api: 'https://api.realnet.io',
   home: 'https://www.realscape.io',
   client_id: 'IEmf5XYQJXIHvWcQtZ5FXbLM' ,
   appVersion: require('../../package.json').version,
